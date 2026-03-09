3a5b53e98577786f747d1d321da336fefbbf9ed7
Maximiliano Marin Avila <maxgoles10@gmail.com>
2026-03-08 19:22:21 -0600

    main.c
    Este codigo prueba el funcionamiento del  TAD nodo

--- nodo/main.c
+++ nodo/main.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #include "nodo.h"
 
-int main()
+int main() 
 {
     printf("Prueba del TAD Nodo\n");
 
-    /* TODO
-       1 Crear un nodo con valor 10
-    */
-    Nodo* n = NULL;
+    Nodo* n = crearNodo(10);
 
-    /* TODO
-       2 Mostrar el valor del nodo usando obtenerValor
-    */
+    if (n != NULL) {
+        printf("Valor actual: %d\n", obtenerValor(n));
+    }
 
-    /* TODO
-       3 Cambiar el valor a 25 usando asignarValor
-    */
+    asignarValor(n, 25);
 
-    /* TODO
-       4 Mostrar nuevamente el valor
-    */
+    if (n != NULL) {
+        printf("Nuevo valor asignado: %d\n", obtenerValor(n));
+    }
 
-    /* TODO
-       5 Liberar el nodo con destruirNodo
-    */
+    destruirNodo(n);
+    n = NULL; // Buena práctica para evitar punteros colgantes
 
     return 0;
 }
